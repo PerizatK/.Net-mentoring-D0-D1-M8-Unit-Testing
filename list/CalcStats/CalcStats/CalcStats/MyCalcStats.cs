1366b8162eeab3bccfaaa610d370f30f30b74433
Perizat <karina.perizat@epam.com>
2021-01-21 14:49:39 +0600

    Calc Stats is ready

---
+++ list/CalcStats/CalcStats/CalcStats/MyCalcStats.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+
+namespace CalcStats
+{
+    public class MyCalcStats
+    {
+        private readonly int[] nums;
+
+        public MyCalcStats(int[] nums)
+        {
+            this.nums = nums;
+        }
+
+        public int GetMininum()
+        {
+            int min = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                min = min < nums[i] ? min : nums[i];
+            }
+            return min;
+        }
+
+        public int GetMaximum()
+        {
+            int max = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                max = max > nums[i] ? max : nums[i];
+            }
+            return max;
+        }
+
+        public int Count()
+        {
+            return nums.Length;
+        }
+
+        public double GetAverage()
+        {
+            double avg = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                avg += nums[i];
+            }
+            avg /= nums.Length;
+            return avg;
+            //return nums.Average()
+        }
+    }
+}
